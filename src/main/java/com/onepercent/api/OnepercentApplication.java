76bb1ffa754c342ec9ff849ab96d8ab7f5a54ef3
henriquehiga <codehiga@gmail.com>
2025-05-26 19:23:14 -0300

    chore: projeto inciiado com java

---
+++ src/main/java/com/onepercent/api/OnepercentApplication.java
@@ -0,0 +1,13 @@
+package com.onepercent.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepercentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepercentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onepercent/api/OnepercentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onepercent.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnepercentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
