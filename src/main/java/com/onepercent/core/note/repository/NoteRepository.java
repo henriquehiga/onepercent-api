5d4201252018fb73ba0b022025f45c73a27e7b81
henriquehiga <codehiga@gmail.com>
2025-06-08 12:53:23 -0300

    feat: Note e Priority repository

---
+++ src/main/java/com/onepercent/core/note/repository/NoteRepository.java
@@ -0,0 +1,7 @@
+package com.onepercent.core.note.repository;
+
+import com.onepercent.core.note.Note;
+import com.onepercent.core.shared.interfaces.CommonRepository;
+
+public interface NoteRepository extends CommonRepository<Note, String> {
+}
---
+++ src/main/java/com/onepercent/core/priority/repository/PriorityRepository.java
@@ -0,0 +1,7 @@
+package com.onepercent.core.priority.repository;
+
+import com.onepercent.core.priority.Priority;
+import com.onepercent.core.shared.interfaces.CommonRepository;
+
+public interface PriorityRepository extends CommonRepository<Priority, String> {
+}
